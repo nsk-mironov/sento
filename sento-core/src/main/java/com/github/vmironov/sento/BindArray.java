469b518057d914e7dd2a7ba93d67c0f91aadca94
Vladimir Mironov <nsk.mironov@gmail.com>
2015-10-30 18:39:18 +0300

    More annotations

---
+++ sento-core/src/main/java/com/github/vmironov/sento/BindArray.java
@@ -0,0 +1,11 @@
+package com.github.vmironov.sento;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.FIELD)
+public @interface BindArray {
+  public int value();
+}
