392a2b914ce33f992b0bbbdd7ac6b54fb9cc4c97
Vladimir Mironov <nsk.mironov@gmail.com>
2015-11-08 18:14:32 +0300

    Delete unused class

--- sento-compiler/src/main/kotlin/io/sento/compiler/model/MethodBindingSpec.kt
+++
@@ -1,9 +0,0 @@
-package io.sento.compiler.model
-
-import io.sento.MethodBinding
-import org.objectweb.asm.Type
-
-internal data class MethodBindingSpec(
-    public val annotation: Type,
-    public val binding: MethodBinding
-)
