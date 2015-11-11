28a8b268701e2196361f53e33eafd0866b92f285
Vladimir Mironov <nsk.mironov@gmail.com>
2015-11-12 02:05:09 +0300

    Minor refactoring

--- sento-compiler/src/main/kotlin/io/sento/compiler/bindings/methods/MethodBindingGeneratorImpl.kt
+++ sento-compiler/src/main/kotlin/io/sento/compiler/bindings/methods/MethodBindingGeneratorImpl.kt
@@ -42,12 +42,12 @@ internal class MethodBindingGeneratorImpl(private val binding: MethodBindingSpec
         adapter.ifNull(this)
 
         adapter.loadLocal(view)
-        adapter.newInstance(listener.generatedType)
+        adapter.newInstance(listener.type)
         adapter.dup()
 
         adapter.loadArg(context.variable("target"))
-        adapter.invokeConstructor(listener.generatedType, Method.getMethod("void <init> (${listener.generatedTarget.className})"))
-        adapter.invokeVirtual(listener.listenerOwner, Method(listener.listenerSetter.name, "(L${listener.listenerType.internalName};)V"))
+        adapter.invokeConstructor(listener.type, Method.getMethod("void <init> (${listener.target.className})"))
+        adapter.invokeVirtual(binding.owner, Method(binding.setter.name, "(L${binding.listener.internalName};)V"))
 
         adapter.mark(this)
       }
@@ -57,7 +57,7 @@ internal class MethodBindingGeneratorImpl(private val binding: MethodBindingSpec
   }
 
   private fun onCreateBindingListener(listener: ListenerSpec, environment: GenerationEnvironment): GeneratedContent {
-    return GeneratedContent(listener.generatedType.toClassFilePath(), environment.createClass {
+    return GeneratedContent(listener.type.toClassFilePath(), environment.createClass {
       visitListenerHeader(listener, environment)
       visitListenerFields(listener, environment)
 
@@ -67,21 +67,21 @@ internal class MethodBindingGeneratorImpl(private val binding: MethodBindingSpec
   }
 
   private fun ClassVisitor.visitListenerHeader(listener: ListenerSpec, environment: GenerationEnvironment) {
-    visit(V1_6, ACC_PUBLIC + ACC_SUPER, listener.generatedType.internalName, null, Types.TYPE_OBJECT.internalName, arrayOf(listener.listenerType.internalName))
+    visit(V1_6, ACC_PUBLIC + ACC_SUPER, listener.type.internalName, null, Types.TYPE_OBJECT.internalName, arrayOf(binding.listener.internalName))
   }
 
   private fun ClassVisitor.visitListenerFields(listener: ListenerSpec, environment: GenerationEnvironment) {
-    visitField(ACC_PRIVATE + ACC_FINAL, "target", listener.generatedTarget.descriptor, null, null)
+    visitField(ACC_PRIVATE + ACC_FINAL, "target", listener.target.descriptor, null, null)
   }
 
   private fun ClassVisitor.visitListenerConstructor(listener: ListenerSpec, environment: GenerationEnvironment) {
-    GeneratorAdapter(ACC_PUBLIC, Method.getMethod("void <init> (${listener.generatedTarget.className})"), null, null, this).apply {
+    GeneratorAdapter(ACC_PUBLIC, Method.getMethod("void <init> (${listener.target.className})"), null, null, this).apply {
       loadThis()
       invokeConstructor(Types.TYPE_OBJECT, Method.getMethod("void <init> ()"))
 
       loadThis()
       loadArg(0)
-      putField(listener.generatedType, "target", listener.generatedTarget)
+      putField(listener.type, "target", listener.target)
 
       returnValue()
       endMethod()
@@ -89,12 +89,12 @@ internal class MethodBindingGeneratorImpl(private val binding: MethodBindingSpec
   }
 
   private fun ClassVisitor.visitListenerCallback(listener: ListenerSpec, environment: GenerationEnvironment) {
-    GeneratorAdapter(ACC_PUBLIC, Method(listener.generatedMethod.name, listener.generatedMethod.type.descriptor), listener.generatedMethod.signature, null, this).apply {
+    GeneratorAdapter(ACC_PUBLIC, Method(listener.callback.name, listener.callback.type.descriptor), listener.callback.signature, null, this).apply {
       loadThis()
-      getField(listener.generatedType, "target", listener.generatedTarget)
+      getField(listener.type, "target", listener.target)
 
       loadArg(0)
-      invokeVirtual(listener.generatedTarget, Method(listener.method.name, "(L${Types.TYPE_VIEW.internalName};)V"))
+      invokeVirtual(listener.target, Method(listener.method.name, "(L${Types.TYPE_VIEW.internalName};)V"))
 
       returnValue()
       endMethod()
@@ -102,28 +102,13 @@ internal class MethodBindingGeneratorImpl(private val binding: MethodBindingSpec
   }
 
   private fun createListenerSpec(context: MethodBindingContext): ListenerSpec {
-    return ListenerSpec(
-        listenerType = binding.listener,
-        listenerOwner = binding.owner,
-        listenerSetter = binding.setter,
-
-        generatedType = context.factory.newAnonymousType(),
-        generatedTarget = context.clazz.type,
-        generatedMethod = binding.callback,
-
-        method = context.method
-    )
+    return ListenerSpec(context.factory.newAnonymousType(), context.clazz.type, binding.callback, context.method)
   }
 
   private data class ListenerSpec(
-      public val listenerType: Type,
-      public val listenerOwner: Type,
-      public val listenerSetter: MethodSpec,
-
-      public val generatedType: Type,
-      public val generatedTarget: Type,
-      public val generatedMethod: MethodSpec,
-
+      public val type: Type,
+      public val target: Type,
+      public val callback: MethodSpec,
       public val method: MethodSpec
   )
 }
