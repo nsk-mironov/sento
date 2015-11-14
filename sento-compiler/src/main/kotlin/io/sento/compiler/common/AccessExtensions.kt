fbd0299c88312c8ad029ea491b57cc403335d29a
Vladimir Mironov <nsk.mironov@gmail.com>
2015-11-14 12:41:05 +0300

    Provide more AccessExtensions

--- sento-compiler/src/main/kotlin/io/sento/compiler/common/AccessExtensions.kt
+++ sento-compiler/src/main/kotlin/io/sento/compiler/common/AccessExtensions.kt
@@ -2,6 +2,15 @@ package io.sento.compiler.common
 
 import org.objectweb.asm.Opcodes
 
+internal val Int.isPublic: Boolean
+  get() = this and Opcodes.ACC_PUBLIC != 0
+
+internal val Int.isPrivate: Boolean
+  get() = this and Opcodes.ACC_PRIVATE != 0
+
+internal val Int.isProtected: Boolean
+  get() = this and Opcodes.ACC_PROTECTED != 0
+
 internal val Int.isInterface: Boolean
   get() = this and Opcodes.ACC_INTERFACE != 0
 
