28a5bece023a2bc8324169229021dcc68b375fd4
Vladimir Mironov <nsk.mironov@gmail.com>
2015-11-14 17:33:33 +0300

    Don't allow arrays in ViewBinding

--- sento-compiler/src/main/kotlin/io/sento/compiler/bindings/fields/ViewBindingGenerator.kt
+++ sento-compiler/src/main/kotlin/io/sento/compiler/bindings/fields/ViewBindingGenerator.kt
@@ -7,7 +7,9 @@ import io.sento.compiler.SentoException
 import io.sento.compiler.common.Annotations
 import io.sento.compiler.common.Methods
 import io.sento.compiler.common.Types
+import io.sento.compiler.common.isInterface
 import io.sento.compiler.common.simpleName
+import org.objectweb.asm.Type
 import org.slf4j.LoggerFactory
 
 internal class ViewBindingGenerator : FieldBindingGenerator {
@@ -23,13 +25,18 @@ internal class ViewBindingGenerator : FieldBindingGenerator {
     logger.info("Generating @{} binding for '{}' field",
         annotation.type.simpleName, field.name)
 
+    if (field.type.sort == Type.ARRAY) {
+      throw SentoException("Unable to generate @{0} binding for ''{1}#{2}\'' field - arrays are not supported, but ''{3}'' was found.",
+          annotation.type.simpleName, clazz.type.className, field.name, field.type.className)
+    }
+
     val optional = field.getAnnotation<Optional>() != null
-    val isInterface = environment.registry.isInterface(field.type)
     val isView = environment.registry.isSubclassOf(field.type, Types.VIEW)
+    val isInterface = environment.registry.reference(field.type).access.isInterface
 
     if (!isInterface && !isView) {
-      throw SentoException("Unable to generate @{0} binding for \"{1}#{2}\" field - it must be a subclass of \"{3}\" or an interface",
-          annotation.type.className, clazz.type.className, field.name, Types.VIEW.className)
+      throw SentoException("Unable to generate @{0} binding for ''{1}#{2}\'' field - it must be a subclass of ''{3}'' or an interface, but ''{4}'' was found.",
+          annotation.type.simpleName, clazz.type.className, field.name, Types.VIEW.className, field.type.className)
     }
 
     adapter.loadArg(context.variable("target"))
