b77975b23e9bac79c037d17eee743f597a126c24
Vladimir Mironov <nsk.mironov@gmail.com>
2015-11-15 23:09:29 +0300

    Get rid of asmifier

--- sento-asmifier/src/main/java/io/sento/Binding.java
+++
@@ -1,7 +0,0 @@
-package io.sento;
-
-public interface Binding<T> {
-  public <S> void bind(final T target, final S source, final Finder<? super S> finder);
-
-  public void unbind(final T target);
-}
