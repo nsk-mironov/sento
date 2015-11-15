82d91e5627a2a005e91e5347badffa45d1d42dd1
Vladimir Mironov <nsk.mironov@gmail.com>
2015-11-15 21:36:12 +0300

    Declare custom annotations in kotlin

---
+++ sento-sample/src/main/java/io/sento/sample/annotations/OnFocusLost.kt
@@ -0,0 +1,23 @@
+package io.sento.sample.annotations
+
+import android.view.View
+import io.sento.ListenerBinding
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@ListenerBinding(
+    owner = "android.view.View",
+    listener = "io.sento.sample.annotations.OnFocusLostListener",
+    setter = "setOnFocusChangeListener"
+)
+annotation class OnFocusLost(vararg val value: Int)
+
+abstract class OnFocusLostListener : View.OnFocusChangeListener {
+  override fun onFocusChange(view: View, focused: Boolean) {
+    if (!focused) {
+      onFocusLost(view)
+    }
+  }
+
+  abstract fun onFocusLost(view: View)
+}
---
+++ sento-sample/src/main/java/io/sento/sample/annotations/OnFocusReceived.kt
@@ -0,0 +1,23 @@
+package io.sento.sample.annotations
+
+import android.view.View
+import io.sento.ListenerBinding
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@ListenerBinding(
+    owner = "android.view.View",
+    listener = "io.sento.sample.annotations.OnFocusReceivedListener",
+    setter = "setOnFocusChangeListener"
+)
+annotation class OnFocusReceived(vararg val value: Int)
+
+abstract class OnFocusReceivedListener : View.OnFocusChangeListener {
+  override fun onFocusChange(view: View, focused: Boolean) {
+    if (focused) {
+      onFocusReceived(view)
+    }
+  }
+
+  abstract fun onFocusReceived(view: View)
+}
