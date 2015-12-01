563cf68bc85bcc19b20b99d4044b38aa9d22e3e5
Vladimir Mironov <nsk.mironov@gmail.com>
2015-12-02 01:33:42 +0300

    Release 0.10.2

--- sento-plugin/src/main/groovy/io/sento/SentoPlugin.groovy
+++ sento-plugin/src/main/groovy/io/sento/SentoPlugin.groovy
@@ -11,7 +11,7 @@ public class SentoPlugin implements Plugin<Project> {
   }
 
   private static void onPrepareDependencies(final Project project) {
-    project.dependencies.add("compile", "io.sento:sento-runtime:0.10.2-SNAPSHOT@aar")
+    project.dependencies.add("compile", "io.sento:sento-runtime:0.10.2@aar")
   }
 
   private static void onPrepareTransforms(final Project project) {
