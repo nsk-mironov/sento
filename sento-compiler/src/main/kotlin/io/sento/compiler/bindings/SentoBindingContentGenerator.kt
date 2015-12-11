bdf2e1fa8d585ced75d421085ceacd93ccbf3083
Vladimir Mironov <nsk.mironov@gmail.com>
2015-12-12 01:13:54 +0300

    More SentoBindingContentGenerator refatoring

--- sento-compiler/src/main/kotlin/io/sento/compiler/bindings/SentoBindingContentGenerator.kt
+++ sento-compiler/src/main/kotlin/io/sento/compiler/bindings/SentoBindingContentGenerator.kt
@@ -24,18 +24,21 @@ import org.objectweb.asm.Opcodes.ACC_STATIC
 import org.objectweb.asm.Opcodes.ACC_SUPER
 import org.objectweb.asm.Opcodes.ACC_SYNTHETIC
 import org.objectweb.asm.Opcodes.ASM5
-import org.slf4j.LoggerFactory
 import java.util.ArrayList
 
 internal class SentoBindingContentGenerator(private val clazz: ClassSpec) : ContentGenerator {
   public companion object {
-    private val logger = LoggerFactory.getLogger(SentoBindingContentGenerator::class.java)
+    public const val EXTRA_BINDING_SPEC = "EXTRA_BINDING_SPEC"
 
     private const val ARGUMENT_TARGET = 0
     private const val ARGUMENT_SOURCE = 1
     private const val ARGUMENT_FINDER = 2
 
-    public const val EXTRA_BINDING_SPEC = "EXTRA_BINDING_SPEC"
+    private val METHOD_BIND_DESCRIPTOR = Methods.get("bind", Types.VOID, Types.OBJECT, Types.OBJECT, Types.FINDER)
+    private val METHOD_BIND_SIGNATURE = "<S:Ljava/lang/Object;>(Ljava/lang/Object;TS;Lio/sento/Finder<-TS;>;)V"
+
+    private val METHOD_UNBIND_DESCRIPTOR = Methods.get("unbind", Types.VOID, Types.OBJECT)
+    private val METHOD_UNBIND_SIGNATURE = null
   }
 
   override fun generate(environment: GenerationEnvironment): Collection<GeneratedContent> {
@@ -43,22 +46,8 @@ internal class SentoBindingContentGenerator(private val clazz: ClassSpec) : Cont
     val result = ArrayList<GeneratedContent>()
 
     if (!binding.bindings.isEmpty() || !binding.listeners.isEmpty()) {
-      logger.info("Generating SentoBinding for '{}' class:", clazz.type.className)
-
-      result.add(GeneratedContent.from(environment.naming.getBindingType(clazz), mapOf(EXTRA_BINDING_SPEC to clazz), environment.newClass {
-        visitHeader(environment)
-        visitConstructor(environment)
-
-        visitBindMethod(binding, environment)
-        visitUnbindMethod(binding, environment)
-      }))
-
-      result.add(GeneratedContent.from(clazz.type, mapOf(), environment.newClass {
-        onCreatePatchedClassForBinding(this, binding, environment)
-        onCreateSyntheticFieldsForListeners(this, binding, environment)
-        onCreateSyntheticFieldsForViews(this, binding, environment)
-        onCreateSyntheticMethodsForListeners(this, binding, environment)
-      }))
+      result.add(onCreateBindingClassGeneratedContent(binding, environment))
+      result.add(onCreatePatchedClassGeneratedContent(binding, environment))
 
       binding.listeners.flatMapTo(result) {
         ListenerBinder().generate(it, environment)
@@ -68,72 +57,81 @@ internal class SentoBindingContentGenerator(private val clazz: ClassSpec) : Cont
     return result
   }
 
-  private fun ClassWriter.visitHeader(environment: GenerationEnvironment) = apply {
-    visit(ACC_PUBLIC + ACC_SUPER, environment.naming.getBindingType(clazz), null, Types.OBJECT, arrayOf(Types.BINDING))
+  private fun onCreateBindingClassGeneratedContent(binding: BindingSpec, environment: GenerationEnvironment): GeneratedContent {
+    return GeneratedContent.from(environment.naming.getBindingType(clazz), mapOf(EXTRA_BINDING_SPEC to clazz), environment.newClass {
+      visit(ACC_PUBLIC + ACC_SUPER, environment.naming.getBindingType(clazz), null, Types.OBJECT, arrayOf(Types.BINDING))
+
+      newMethod(ACC_PUBLIC, Methods.getConstructor()) {
+        loadThis()
+        invokeConstructor(Types.OBJECT, Methods.getConstructor())
+      }
+
+      newMethod(ACC_PUBLIC, METHOD_BIND_DESCRIPTOR, METHOD_BIND_SIGNATURE) {
+        val variables = VariablesContext()
+        val method = this
+
+        onCreateLocalVariablesFromArgs(method, binding, variables, environment)
+        onCreateLocalVariablesForViews(method, binding, variables, environment)
+        onEnforceRequiredViewTargets(method, binding, variables, environment)
+
+        onBindViewTargetFields(method, binding, variables, environment)
+        onBindSyntheticViewFields(method, binding, variables, environment)
+        ListenerBinder().bind(binding.listeners, variables, method, environment)
+      }
+
+      newMethod(ACC_PUBLIC, METHOD_UNBIND_DESCRIPTOR, METHOD_UNBIND_SIGNATURE) {
+        val variables = VariablesContext()
+        val method = this
+
+        onCreateLocalVariablesFromArgs(method, binding, variables, environment)
+        ListenerBinder().unbind(binding.listeners, variables, method, environment)
+        onUnbindSyntheticViewFields(method, binding, variables, environment)
+        onUnbindViewTargetFields(method, binding, variables, environment)
+      }
+    })
   }
 
-  private fun ClassWriter.visitConstructor(environment: GenerationEnvironment) {
-    newMethod(ACC_PUBLIC, Methods.getConstructor()) {
-      loadThis()
-      invokeConstructor(Types.OBJECT, Methods.getConstructor())
+  private fun onCreatePatchedClassGeneratedContent(binding: BindingSpec, environment: GenerationEnvironment): GeneratedContent {
+    return GeneratedContent.from(clazz.type, mapOf(), environment.newClass {
+      onCreatePatchedClassForBinding(this, binding, environment)
+      onCreateSyntheticFieldsForListeners(this, binding, environment)
+      onCreateSyntheticFieldsForViews(this, binding, environment)
+      onCreateSyntheticMethodsForListeners(this, binding, environment)
+    })
+  }
+
+  private fun onCreateLocalVariablesFromArgs(adapter: GeneratorAdapter, binding: BindingSpec, variables: VariablesContext, environment: GenerationEnvironment) {
+    variables.variable("target", adapter.newLocal(clazz.type).apply {
+      adapter.loadArg(ARGUMENT_TARGET)
+      adapter.checkCast(clazz.type)
+      adapter.storeLocal(this)
+    })
+  }
+
+  private fun onCreateLocalVariablesForViews(adapter: GeneratorAdapter, binding: BindingSpec, variables: VariablesContext, environment: GenerationEnvironment) {
+    binding.views.distinctBy { it.id }.forEach {
+      variables.variable("view${it.id}", adapter.newLocal(Types.VIEW).apply {
+        adapter.loadArg(ARGUMENT_FINDER)
+        adapter.push(it.id)
+
+        adapter.loadArg(ARGUMENT_SOURCE)
+        adapter.invokeInterface(Types.FINDER, Methods.get("find", Types.VIEW, Types.INT, Types.OBJECT))
+
+        adapter.storeLocal(this)
+      })
     }
   }
 
-  private fun ClassWriter.visitBindMethod(binding: BindingSpec, environment: GenerationEnvironment) {
-    val descriptor = Methods.get("bind", Types.VOID, Types.OBJECT, Types.OBJECT, Types.FINDER)
-    val signature = "<S:Ljava/lang/Object;>(Ljava/lang/Object;TS;Lio/sento/Finder<-TS;>;)V"
+  private fun onEnforceRequiredViewTargets(adapter: GeneratorAdapter, binding: BindingSpec, variables: VariablesContext, environment: GenerationEnvironment) {
+    binding.views.filter { !it.optional }.distinctBy { it.id }.forEach {
+      adapter.loadArg(ARGUMENT_FINDER)
+      adapter.push(it.id)
 
-    newMethod(ACC_PUBLIC, descriptor, signature) {
-      val variables = VariablesContext()
+      adapter.loadLocal(variables.view(it.id))
+      adapter.loadArg(ARGUMENT_SOURCE)
+      adapter.push(it.owner.name)
 
-      variables.variable("target", newLocal(clazz.type).apply {
-        loadArg(ARGUMENT_TARGET)
-        checkCast(clazz.type)
-        storeLocal(this)
-      })
-
-      binding.views.distinctBy { it.id }.forEach {
-        variables.variable("view${it.id}", newLocal(Types.VIEW).apply {
-          loadArg(ARGUMENT_FINDER)
-          push(it.id)
-
-          loadArg(ARGUMENT_SOURCE)
-          invokeInterface(Types.FINDER, Methods.get("find", Types.VIEW, Types.INT, Types.OBJECT))
-
-          storeLocal(this)
-        })
-      }
-
-      binding.views.filter { !it.optional }.distinctBy { it.id }.forEach {
-        loadArg(ARGUMENT_FINDER)
-        push(it.id)
-
-        loadLocal(variables.view(it.id))
-        loadArg(ARGUMENT_SOURCE)
-        push(it.owner.name)
-
-        invokeInterface(Types.FINDER, Methods.get("require", Types.VOID, Types.INT, Types.VIEW, Types.OBJECT, Types.STRING))
-      }
-
-      onBindViewTargetFields(this, binding, variables, environment)
-      onBindSyntheticViewFields(this, binding, variables, environment)
-      ListenerBinder().bind(binding.listeners, variables, this, environment)
-    }
-  }
-
-  private fun ClassWriter.visitUnbindMethod(binding: BindingSpec, environment: GenerationEnvironment) {
-    newMethod(ACC_PUBLIC, Methods.get("unbind", Types.VOID, Types.OBJECT)) {
-      val variables = VariablesContext()
-
-      variables.variable("target", newLocal(clazz.type).apply {
-        loadArg(ARGUMENT_TARGET)
-        checkCast(clazz.type)
-        storeLocal(this)
-      })
-
-      ListenerBinder().unbind(binding.listeners, variables, this, environment)
-      onUnbindSyntheticViewFields(this, binding, variables, environment)
-      onUnbindViewTargetFields(this, binding, variables, environment)
+      adapter.invokeInterface(Types.FINDER, Methods.get("require", Types.VOID, Types.INT, Types.VIEW, Types.OBJECT, Types.STRING))
     }
   }
 
